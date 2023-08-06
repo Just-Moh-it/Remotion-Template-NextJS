fe5bcf807047b4335c13afea95d3d542490e35ef
Mohit Yadav <www.mohit2004@gmail.com>
2023-08-06 21:26:23 +0530

    chore: trigger deploy

--- remotion/webpackOverride.ts
+++ remotion/webpackOverride.ts
@@ -1,6 +1,5 @@
 import { WebpackOverrideFn } from "@remotion/cli/config";
 import { enableTailwind } from "@remotion/tailwind";
-
 export const webpackOverride: WebpackOverrideFn = (currentConfiguration) => {
   return enableTailwind({
     ...currentConfiguration,
